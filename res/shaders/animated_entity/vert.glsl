07aabcc037e2d825964af0b288a5b11c629a0bca
KhalinGH <65938616+KhalinGH@users.noreply.github.com>
2023-05-16 22:51:58 +0800

    updated frag shader for animated entity

--- res/shaders/animated_entity/vert.glsl
+++ res/shaders/animated_entity/vert.glsl
@@ -10,8 +10,9 @@ layout(location = 3) in vec4 bone_weights;
 layout(location = 4) in uvec4 bone_indices;
 
 out VertexOut {
-    LightingResult lighting_result;
     vec2 texture_coordinate;
+    vec3 ws_position;
+    vec3 ws_normal;
 } vertex_out;
 
 // Per instance data
@@ -59,23 +60,23 @@ void main() {
     mat4 animation_matrix = model_matrix * bone_transform;
     mat3 normal_matrix = cofactor(animation_matrix);
 
-    vec3 ws_position = (animation_matrix * vec4(vertex_position, 1.0f)).xyz;
-    vec3 ws_normal = normalize(normal_matrix * normal);
+    vertex_out.ws_position = (animation_matrix * vec4(vertex_position, 1.0f)).xyz;
+    vertex_out.ws_normal = normalize(normal_matrix * normal);
     vertex_out.texture_coordinate = texture_coordinate;
 
-    gl_Position = projection_view_matrix * vec4(ws_position, 1.0f);
+    gl_Position = projection_view_matrix * vec4(vertex_out.ws_position, 1.0f);
 
     // Per vertex light calcs are below this point
-    vec3 ws_view_dir = normalize(ws_view_position - ws_position);
-    LightCalculatioData light_calculation_data = LightCalculatioData(ws_position, ws_view_dir, ws_normal);
-    Material material = Material(diffuse_tint, specular_tint, ambient_tint, shininess);
+    //vec3 ws_view_dir = normalize(ws_view_position - ws_position);
+    //LightCalculatioData light_calculation_data = LightCalculatioData(ws_position, ws_view_dir, ws_normal);
+    //Material material = Material(diffuse_tint, specular_tint, ambient_tint, shininess);
 
-    vertex_out.lighting_result = total_light_calculation(light_calculation_data, material
-        #if NUM_PL > 0
-        ,point_lights
-        #endif
-        #if NUM_DL > 0
-        ,directional_lights
-        #endif
-    );
+    //vertex_out.lighting_result = total_light_calculation(light_calculation_data, material
+      //  #if NUM_PL > 0
+        //,point_lights
+        //#endif
+        //#if NUM_DL > 0
+        //,directional_lights
+        //#endif
+    //);
 }
